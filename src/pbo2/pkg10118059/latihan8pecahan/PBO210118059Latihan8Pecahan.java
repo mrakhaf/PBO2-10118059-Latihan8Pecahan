c3e5d074e46d5964a5a024bf4bf5fdb6fbdc5042
mrakhaf <48128093+mrakhaf@users.noreply.github.com>
2019-09-26 17:10:40 +0700

    Add files via upload

---
+++ src/pbo2/pkg10118059/latihan8pecahan/PBO210118059Latihan8Pecahan.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118059.latihan8pecahan;
+
+/**
+ *
+ * @author 
+ * Nama  : Muhammad Rakha Firdaus
+ * NIM   : 10118059
+ * Kelas : IF-2
+ */
+public class PBO210118059Latihan8Pecahan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        float f;
+        double d; 
+        f = 1234567890.123456789F;
+        d = 1_234_567_890.123456789;
+        System.out.println("Float : " + f);
+        System.out.println("Double : " + d);
+    }
+    
+}
